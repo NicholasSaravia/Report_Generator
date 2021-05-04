e8b8631ad60a95b416520a6452554c88c4c14ffb
Nicholas Saravia <NicholasSaravia@users.noreply.github.com>
2021-05-04 09:13:02 -0400

    moved upload method outside of git component for reusability

---
+++ ReportGenerator/Pages/Index.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace ReportGenerator.Pages
+{
+    public partial class Index
+    {
+        IList<IBrowserFile> files = new List<IBrowserFile>();
+        private void UploadFiles(InputFileChangeEventArgs e)
+        {
+            // add readonly files to a list
+            foreach (var file in e.GetMultipleFiles())
+            {
+                files.Add(file);
+            }
+
+            // parse data
+
+        }
+
+        private void ParseData()
+        {
+
+        }
+    }
+}
