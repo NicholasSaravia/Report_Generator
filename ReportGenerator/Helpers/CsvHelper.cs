ad03a23f402f73d086eb4efa23435f6736b5e991
Nicholas Saravia <NicholasSaravia@users.noreply.github.com>
2021-05-04 16:06:23 -0400

    created csvHelper class to parse csv file

---
+++ ReportGenerator/Helpers/CsvHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyCsvParser;
+using TinyCsvParser.Mapping;
+
+namespace ReportGenerator.Helpers
+{
+    public class CsvHelper
+    {
+        public static List<T> ReadCsv<T>(ICsvMapping<T> mapping, string path)
+        {
+            CsvParserOptions csvParserOptions = new CsvParserOptions(true, ',');
+            CsvParser<T> csvParser = new CsvParser<T>(csvParserOptions, mapping);
+            var results = csvParser.ReadFromFile(path, encoding: Encoding.ASCII);
+
+            return results.Select(x => x.Result).ToList();
+        }
+    }
+}
